cb413317f24c09168b5b3b560143f4ae0384a530
Bayer iMac Onyx <bayerimac4@Gauravs-Mac.local>
2018-06-12 10:29:35 +0530

    initial commit

---
+++ GeoLocatorFramework.h
@@ -0,0 +1,19 @@
+//
+//  GeoLocatorFramework.h
+//  GeoLocatorFramework
+//
+//  Created by Bayer iMac Onyx on 28/05/18.
+//  Copyright © 2018 wipro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GeoLocatorFramework.
+FOUNDATION_EXPORT double GeoLocatorFrameworkVersionNumber;
+
+//! Project version string for GeoLocatorFramework.
+FOUNDATION_EXPORT const unsigned char GeoLocatorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GeoLocatorFramework/PublicHeader.h>
+
+
